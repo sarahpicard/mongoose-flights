77d0fbe5bda2deb8f2a9b226572c7e501319ee56
sarahpicard <sarahannrey225@gmail.com>
2022-02-28 16:16:23 -0500

    structure partial views

--- models/flight.js
+++ models/flight.js
@@ -2,11 +2,19 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema
 
+const ticketSchema = new Schema ({
+  seat: {type: String, match: /[A-F][1-9]\d?/},
+  price: Number
+}, {
+  timestamps: true
+})
+
 const flightSchema = new Schema ({
   airline: String, 
   airport: String, 
   flightNo: Number, 
   departs: Date, 
+  tickets: [ticketSchema]
 })
 
 const Flight = mongoose.model('Flight', flightSchema)
--- routes/flights.js
+++ routes/flights.js
@@ -8,6 +8,9 @@ router.get('/', flightsCtrl.index)
 // GET localhost:3000/new
 router.get('/new', flightsCtrl.new)
 
+// GET localhost:3000/:id
+router.get('/:id', flightsCtrl.show)
+
 // POST localhost:3000
 router.post('/', flightsCtrl.create)
 
